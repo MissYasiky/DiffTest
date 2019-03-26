1ac06cae873d4d985f42c8ffb70555615a99af7e
MissYasiky <gzxieyunjia@gmail.com>
2019-03-26 22:01:22 +0800

    change file: main.c

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+void printArray(int array[], int count);
+
 int main () {
-	printf("Hello world!");
+	int a[] = {22,4,8,32,19,52,1,77,0,2,25};
+	int count = sizeof(a)/sizeof(a[0]);
+	printArray(a, count);
 	return 0;
+}
+
+void printArray(int array[], int count) {
+	for (int i = 0; i < count; i++) {
+		printf("%d ", array[i]);
+	}
+	printf("\n");
 }
